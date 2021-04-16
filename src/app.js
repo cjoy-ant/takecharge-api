8d3f8cc1991f292b69f4a47e5088397c19b75364
Christine Antonio <christinejm.antonio@gmail.com>
2021-04-16 16:25:10 -0700

    setup cors for client origin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const { NODE_ENV } = require("./config");
+const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
 const errorHandler = require("./error-handler");
 
 const app = express();
@@ -12,7 +12,9 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}));
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
